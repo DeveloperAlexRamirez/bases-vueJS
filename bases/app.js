5e711010f94ab8ae8cadc298c6787e2f3098f16d
DeveloperAlexRamirez <alexkey0516@gmail.com>
2021-08-26 13:39:12 -0500

    v-for added

---
+++ bases/app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+  // template: `
+  //       <h1>Hola mundo</h1>
+  //       <p>{{ true ? 'Activo' : 'Inactivo' }}</p>
+  //   `,
+
+  // Component State
+  data() {
+    return {
+      quote: 'What time it is?',
+      author: 'Peter',
+    };
+  },
+
+  methods: {
+    changeQuote() {
+      this.author = 'Alex';
+      this.capitalize();
+    },
+    capitalize() {
+      this.quote = this.quote.toUpperCase();
+    },
+  },
+});
+
+app.mount('#myApp');
